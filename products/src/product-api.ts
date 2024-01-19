27f232bf68ea5a249ff28b284a2cec4db053e100
linhnv2nm <muadongyeuthuong3x@gmail.com>
2024-01-19 08:14:19 +0700

    update

--- products/src/product-api.ts
+++ products/src/product-api.ts
@@ -1,8 +1,32 @@
 // lambda handler 
 
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {ErrorResponse} from "./utility/response";
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+
+
+    const isRoot = event.pathParameters === null;
+
+    switch (event.httpMethod.toLowerCase()) {
+        case "post":
+            if (isRoot) {
+
+            }
+            break;
+        case "get":
+
+        case "put":
+            if (!isRoot) {
+
+            }
+        case "delete":
+            if (!isRoot) {
+
+            }
+    }
+    
+    return ErrorResponse(4)
     return {
         statusCode: 200,
         body: JSON.stringify({
